524b9e0773eb71d780c3809df912138caf03735f
Vu Huy <vuhuycto@gmail.com>
2022-06-16 23:02:01 +0700

    Initial Commit

---
+++ Video Player/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Video Player
+//
+//  Created by Vu Huy on 16/06/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Video Player/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Video Player
+//
+//  Created by Vu Huy on 16/06/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
